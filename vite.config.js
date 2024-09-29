5399004f4e94ddf70f4eb2860303101438744dca
29deepanshutyagi <29deepanshutyagi@gmail.com>
2024-09-29 20:00:45 +0530

    update .gitignore to exclude Vercel files; refactor imports in vite.config.js and add Rollup external configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -10,4 +10,9 @@ export default defineConfig({
     },
   },
   base: "./",
+  build: {
+    rollupOptions: {
+      external: ['#minpath']
+    }
+  }
 });
